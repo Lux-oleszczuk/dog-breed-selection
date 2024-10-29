76f18598405af0270db25f12b0cbdde3531e5207
Lux-oleszczuk <luxana.oleszczuk@student.norwichuni.ac.uk>
2024-10-29 12:02:49 +0000

    functions
    I simplified my functions by removing a repeatable code and make another small async function that holds it.

--- js/database.js
+++ js/database.js
@@ -3,50 +3,55 @@ const dogImage = document.getElementById("image");
 const breedSelect = document.getElementById("breed-select");
 const subBreedSelect = document.getElementById("sub-breed-select");
 
-async function fetchRandomDog() {
+/**
+ * 
+ * @param {string} url url to be fetched
+ */
+
+async function fetchFromAPI(url) {
     try {
-        const randomUrl = "https://dog.ceo/api/breeds/image/random";
-
-        const response = await fetch(randomUrl);
-
+        const response = await fetch(url);
         if(!response.ok) {
-            throw new error("response status: " + response.status);
+            throw new Error("response status: " + response.status);
         }
-
         const json = await response.json();
-        const message = json.message;
 
-        dogImage.src = message;
-    }
-    catch (error) { 
+        return json.message;
+
+    } catch (error) {
         console.log(error);
     }
+
 }
+
+async function fetchRandomDog() {
+    const randomUrl = "https://dog.ceo/api/breeds/image/random";
+
+    const response = await fetch(randomUrl);
+
+    if(!response.ok) {
+        throw new error("response status: " + response.status);
+    }
+
+    const json = await response.json();
+    const message = json.message;
+
+    dogImage.src = message;
+}
+
 
 async function fetchBreedPossibilities() {
     const breedListUrl = "https://dog.ceo/api/breeds/list";
 
-    try {
-        const response = await fetch(breedListUrl);
+    const breedList = await fetchFromAPI(breedListUrl);
 
-        if(!response.ok) {
-            throw new error("response status: " + response.status);
-        }
-
-        const json = await response.json();
-
-        const breedList = json.message;
-
-        for(const breed of breedList) {
-            const newOption = document.createElement("option");
-            newOption.text = breed;
-            breedSelect.options.add(newOption, breed);
-        }
-
-        console.log(json);
-    } catch(error) {
-        console.error(error);
+    for(const breed of breedList) {
+        const newOption = document.createElement("option");
+        newOption.text = breed;
+        breedSelect.options.add(newOption, breed);
     }
+
+
 }
 // link buttons to events
 randomDogButton.onclick = fetchRandomDog;
