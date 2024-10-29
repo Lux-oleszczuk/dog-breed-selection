9ff18d1eb75aace3acba039fe28d5648dd3d6d94
Lux-oleszczuk <luxana.oleszczuk@student.norwichuni.ac.uk>
2024-10-29 11:49:47 +0000

    random button
    made a random button that change an image on a click

--- js/database.js
+++ js/database.js
@@ -1,5 +1,27 @@
+const randomDogButton = document.getElementById("random-button");
+const dogImage = document.getElementById("image");
 const breedSelect = document.getElementById("breed-select");
 const subBreedSelect = document.getElementById("sub-breed-select");
+
+async function fetchRandomDog() {
+    try {
+        const randomUrl = "https://dog.ceo/api/breeds/image/random";
+
+        const response = await fetch(randomUrl);
+
+        if(!response.ok) {
+            throw new error("response status: " + response.status);
+        }
+
+        const json = await response.json();
+        const message = json.message;
+
+        dogImage.src = message;
+    }
+    catch (error) { 
+        console.log(error);
+    }
+}
 
 async function fetchBreedPossibilities() {
     const breedListUrl = "https://dog.ceo/api/breeds/list";
@@ -26,5 +48,8 @@ async function fetchBreedPossibilities() {
         console.error(error);
     }
 }
+// link buttons to events
+randomDogButton.onclick = fetchRandomDog;
 
+fetchRandomDog();
 fetchBreedPossibilities();
